8c08d0600ae12beafc2ed171a333b32b323726a3
caleb-git <calebgithub@yahoo.com>
2023-06-15 17:45:29 -0500

    Juego terminado y listo

--- game.js
+++ game.js
@@ -5,14 +5,36 @@ const btnUp = document.querySelector('#up');  //////////3
 const btnLeft = document.querySelector('#left');////////3 
 const btnRight = document.querySelector('#right');//////3 
 const btnDown = document.querySelector('#down');////////3 
+const spanLives = document.querySelector('#lives'); //9.0
+const spanTime = document.querySelector('#time'); ///10.0
+const spanRecord = document.querySelector('#record'); //11.7
+const pResult = document.querySelector('#result'); //11.7
 
 let canvasSize;///////////////////////////////////////1.5
 let elementSize; ////////////////////////////////////1.11
+let level = 0; //////////////////////////////////////7.2
+let lives = 3; ///////////////////////////////////////8.1
+
+
+let timeStart; //////////////////////////////////////10.1
+let timePlayer; ///////////////////////////////////////////////////////////////****** */
+let timeInterval; ///////////////////////////////////10.5
 
 const playerPosition = {     ////////////////////////3.1                   
     x:undefined,
     y:undefined,
 };
+
+const giftPosition = { ////////////////////////////////5
+    x:undefined,
+    y:undefined,
+};
+
+
+let enemyPositions = []; /////////////////////////////6.0
+
+
+
 
 window.addEventListener('load', setCanvasSize); /////1.2
 window.addEventListener('resize', setCanvasSize);////1.3
@@ -20,16 +42,20 @@ window.addEventListener('resize', setCanvasSize);////1.3
 function setCanvasSize(){////////////////////////////1.4
 
     if(window.innerHeight > window.innerWidth){//////1.6
-        canvasSize = window.innerWidth * 0.8; ///////1.7
+        canvasSize = window.innerWidth * 0.7; ///////1.7
     }else{
-        canvasSize = window.innerHeight * 0.8; //////1.8
+        canvasSize = window.innerHeight * 0.7; //////1.8
     }
+
+    canvasSize = Number(canvasSize.toFixed(0));
 
     canvas.setAttribute('width', canvasSize); ///////1.9
     canvas.setAttribute('height', canvasSize); /////1.10
 
     elementSize = canvasSize / 10; /////////////////1.12
 
+    playerPosition.x = undefined;
+    playerPosition.y = undefined;
     startGame(); ///////////////////////////////////1.13
 }
 
@@ -39,12 +65,28 @@ function startGame(){/////////////////////////////////2
     game.font= elementSize + 'px Verdana'; //////////2.1 
     game.textAlign = 'end';//////////////////////////2.2
 
-    const map = maps[0]; ////////////////////////////2.3
+    const map = maps[level]; ////////////////////////////2.3 y 7.5
+
+    if(!map){ //////////////////////////////////////////7.6
+       gameWin(); //////////////////////////////////////7.7
+       return; /////////////////////////////////////////7.7
+    }
+
+    if(!timeStart){ /////////////////////////////////////10.2
+        timeStart = Date.now(); /////////////////////////10.2
+        timeInterval = setInterval(showTime, 100); //////10.6
+        showRecord(); ///////////////////////////////////11.12
+    }
+
+
+
     const mapRows = map.trim().split('\n'); /////////2.4
     const mapRowCols = mapRows.map(row => row.trim().split('')); /////2.5
     console.log(map, mapRows, mapRowCols); ///////////////////////////2.6
 
-    
+    showlives(); //////////////////////////////////////////////////////9.1
+
+    enemyPositions = []; //////////////////////////////////////////////6.4
     game.clearRect(0,0, canvasSize, canvasSize); //////////////////////3.2    
 
     mapRowCols.forEach((fila, filaI) => { ////////////////////////////2.7
@@ -61,6 +103,15 @@ function startGame(){/////////////////////////////////2
                     playerPosition.y = posY; /////////////////////////3.6
                     console.log({playerPosition}); ///////////////////3.7
                 }
+                
+            } else if(columna == 'I'){ ///////////////////////////////5.1
+                giftPosition.x = posX; ///////////////////////////////5.2
+                giftPosition.y = posY; ///////////////////////////////5.3
+            }else if(columna == 'X'){ ////////////////////////////////6.1
+                enemyPositions.push({ /////////////////////////////// 6.2
+                    x: posX, //////////////////////////////////////// 6.3
+                    y: posY, //////////////////////////////////////// 6.3
+                });
             }
 
 
@@ -83,7 +134,96 @@ function startGame(){/////////////////////////////////2
 //clase9 mover jugador
 
 function movePlayer(){
+    const giftCollisionX = playerPosition.x.toFixed(3) == giftPosition.x.toFixed(3); ///5.4
+    const giftCollisionY = playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3); ///5.4
+    const giftCollision = giftCollisionX && giftCollisionY; ////////////////////////////5.5
+
+    if(giftCollision){ ////////////////////////////////////////////////////////////////5.6
+        /*console.log('Subiste de nivel');*/ ////////////////////////////////////////////5.7
+        levelWin(); /////////////////////////////////////////////////////////////////////7.0
+    }
+
+
+    const enemyCollision = enemyPositions.find(enemy => { //////////////////////////////6.5
+        const enemyCollsionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3); //////6.6
+        const enemyCollsionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3); //////6.7
+        return enemyCollsionX && enemyCollsionY; ///////////////////////////////////////6.8
+    });
+
+    if(enemyCollision){  ///////////////////////////////////////////////////////////////6.9
+        /*console.log('Chocaste contra un enemigo :( ');*/ ////////////////////////////////6.10
+
+        levelFail(); /////////////////////////////////////////////////////////////////////8.0
+        
+    }
+
+
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y); //4
+}
+
+function levelWin(){ //////////////////////////////////////////////////////////////////7.1
+    console.log('Subiste de nivel');
+    level++; //////////////////////////////////////////////////////////////////////////7.3
+    startGame(); //////////////////////////////////////////////////////////////////////7.4
+}
+
+function levelFail(){
+    console.log('Chocaste contra un enemigo :( ');
+    lives--; //////////////////////////////////////////////////////////////////////////8.2
+
+
+    if(lives <= 0){ ///////////////////////////////////////////////////////////////////8.4
+        console.log('lives');
+        level = 0; ////////////////////////////////////////////////////////////////////8.5
+        lives = 3; ////////////////////////////////////////////////////////////////////8.5
+        timeStart = undefined; ///////////////////////////////////////////////////////10.8
+    }
+
+   
+    playerPosition.x = undefined; /////////////////////////////////////////////////////////8.3
+    playerPosition.y = undefined; /////////////////////////////////////////////////////////8.3
+    startGame(); /////////////////////////////////////////////////////////////////////////8.3
+    
+}
+
+function gameWin(){ ////////////////////////////////////////////////////////////////7.8
+    console.log('Terminaste el juego!');
+
+    clearInterval(timeInterval); ///////////////////////////////////////////////////10.7
+
+    const recordTime = localStorage.getItem('record_time'); ////////////////////////11.0
+    const playerTime = Date.now() - timeStart; /////////////////////////////////////11.1
+
+
+    if(recordTime){ ////////////////////////////////////////////////////////////////11.2
+        
+        if(recordTime >= playerTime){ //////////////////////////////////////////////11.3
+            localStorage.setItem('record_time', playerTime); ///////////////////////11.3
+            pResult.innerHTML = 'Superaste el record'; /////////////////////////////11.8
+        }else{ /////////////////////////////////////////////////////////////////////11.4
+            pResult.innerHTML = 'No superaste el record'; //////////////////////////11.9
+        }
+    }else{
+        localStorage.setItem('record_time', playerTime); ////////////////////////////11.5
+        pResult.innerHTML = 'Record Guardado 1° vez'; ///////////////////////////////11.10
+    }
+    console.log({recordTime, playerTime}); //////////////////////////////////////////11.6
+}
+
+function showlives(){ ///////////////////////////////////////////////////////////9.2
+    const heartArray = Array(lives).fill(emojis['HEART']) ///////////////////////9.3
+    console.log(heartArray); ///////////////////////////////////////////////////9.4
+
+    spanLives.innerHTML = ""; //////////////////////////////////////////////////9.6
+    heartArray.forEach(heart => spanLives.append(heart)); //////////////////////9.5
+}
+
+function showTime(){ //////////////////////////////////////////////////////////10.3
+    spanTime.innerHTML = Date.now() - timeStart; //////////////////////////////10.4
+}
+
+function showRecord(){
+    spanRecord.innerHTML = localStorage.getItem('record_time'); /////////////11.13
 }
 
 window.addEventListener('keydown', moveByKeys); //////////////////////////4.1
@@ -122,7 +262,7 @@ function moveUp(){
 }
 
 function moveLeft(){
-    console.log('Me quiero mover hacia la izquierda');
+   console.log('Me quiero mover hacia la izquierda');
 
     if((playerPosition.x - elementSize) < elementSize){ ////////////////////4.5
         console.log('out');
@@ -133,7 +273,7 @@ function moveLeft(){
 }
 
 function moveRight(){
-    console.log('Me quiero mover hacia la derecha');
+   console.log('Me quiero mover hacia la derecha');
 
     if((playerPosition.x + elementSize) > canvasSize){ ////////////////////4.7
         console.log('out');
--- notas.js
+++ notas.js
@@ -29,6 +29,7 @@ arr.forEach(nombre, index, array) => {
     // tu iterador
 };
 
+//Clase 7 minuto 15:50 resumen
 2.7 Recorres el array mapRowCols con el forEach y le das 2 parametros: nombre='fila' e indice.
 2.8 Recorres el array 'fila' con el forEach y le das 2 parametros: nombre='columna' e indice.
 2.9 Sacar el emoji del Objeto [Emojis] que esta en maps.js 
@@ -80,3 +81,150 @@ height= altura rectangulo
     mayor a lo que mide el canvaSize. Di 'out'. Se saliooo
 
 4.8 Mientras no sea mayor al canvaSize ejecuta la funcion startGame
+
+
+//Clase #12 COLISIONES
+5   Creamos la posicion del regalito gift
+5.1 Preguntar sí nos encontramos una 'I' que es un regalito'🎁',
+5.2 PosX significa renderizar en esa posicion de x. 
+5.3 PosY significa renderizar en esa posicion de y.
+    la coordenada quedamos en x,y es la parte 'end' de cada elementooo o cuadrito,
+    estamos buscando que choquen o coalicionen.
+5.4 Sí coincide la posicion de mi player y mi regalito
+    ponle toFixed(decimales), porque a veces no funciona por un decimalito el tamaño de pantalla.
+5.5 Parametro para meterlo al if()
+5.6 Parametro
+5.7 Si el parametro es 'true' o verdad escribe 'subiste de nivel en consola'.
+
+//Clase #13
+
+6.0   Creas un array para meter todas las coordenadas de todas la bombas
+6.1 Si te encuentras una bomba o 'X'
+6.2 Trackea/rastrea todas las coordenadas de bomba o 'X' AL ARRAY QUE SE CREO EN EL 6.0
+6.3 Trakea todas la x: va ser igual posX (osea la coordenada que se encuentra) de todas las bombas
+    Trakea todas la y: va ser igual posY (osea la coordenada que se encuentra) de todas las bombas
+6.4 Limpiamos el array 6.0 porque cada vez que movemos la calaberita vuelve arecorrer el la funcion
+    startGame(); limpiamos para que vuelva trakear todas las bombas.
+6.5 Creamos una constante para saber si choca nuestra calaberita con una bomba. Con .find encontramos
+    al array de enemyPositions que son las bombas y vamos a buscar los guiente:
+6.6 Sí la coordenada de alguna bomba en 'x' es igual a la coordenda de mi jugador o playerPosition en 'x'
+    llamalo enemyCollisionX. Ponle toFixed(3) paraq ue solo te de 3 decimales al trakear coordenadas, 
+    porque la pantalla se mue ve.
+6.7 Sí la coordenada de alguna bomba en 'y' es igual a la coordenda de mi jugador o playerPosition en 'y'
+    llamalo enemyCollisionY. Ponle toFixed(3) paraq ue solo te de 3 decimales, al trakear coordenadas
+    porque la pantalla se mue ve.
+6.8 Sí enemyCollisionX y enemyCollisionY son iguales mi playerPosition, Regresamelos y por
+    lo tato mi enemyCollision del 6.5 es, true.
+6.9 Si enemyCollision es 'True' del 6.8
+6.10 Escribe 'chocaste con un enemigo'
+
+
+#Clase14
+
+7.0 Si el parametro es 'true' ejecuta la funcion levelWin() o subir nivel.
+7.1 Creo la funcion levelWin()
+7.2 Creo la variable level = 0, mis mapas inician en 0.
+7.3 A mi variable le sumo 1, para que cambie de mapa cada vez que toque el regalito.
+7.4 Cuando suba de nivel o toque regalito vuelva a ejecutar la funcion startGame()
+7.5 Antes era const map = maps[0] y ahora se remplazo el o por level asi. const map = maps[level];
+7.6 Si ya se terminaron los mapas
+7.7 Ejecuta la funcion gameWin() y return significa = detener startGame(), para que ya no se
+    ejecuten las funciones 
+7.8 Creo la funcion gameWin()
+
+#Clase 15
+
+8.0 Si colisiona ejecuta la funcion levelFail
+8.1 Vidas por juego
+8.2 Cada vez que choque es una vida MediaElementAudioSourceNode; lives = lives -1
+8.3 En cuanto choque la posicion x-y ponle undefined y ejecuta startGame() para que vuelva iniciar.
+    en 3.4 se le vuelve a dar las coordenadas de inici000.
+8.4 Si las vidas es menor a 0 o es igual a 0
+8.5 Regresalo al nivel 0 y dale 3 vidas otra vez.
+
+#Clase 16
+
+9.0 Traerte el parrafo lives del html, donde se van mostrar las vidas
+9.1 Aqui le decimos que ejecute la funcion showlives(), que sea despues del return del 7.7
+9.2 Creas la funcion showlives()
+9.3 La palabra Array es un constructor de arrays, estamos pidiendo que haga un array
+ de las videas o lives 8.1, y con .fill le estamos pidiendo que lo rellene con corazones.
+9.4 Ver los corazones en console.log
+9.5 Con .forEach estamos pidiendo que recorra el array de corazones, y por cada heart ó elemento
+    que se encuentre, que en el parrafo del html 'spanLives' escriba el elemento o corazon 
+    que se encontro.
+9.6 Antes que recorra el array 9.5 le vamos apedir que se limpie.
+
+#Clase 17 Sistema de Tiempos
+
+Metodosss
+*El 'hola' queremos que escriba cada segundo por 1000 milisegundos:
+setInterval(() => console.log('hola'), 1000);
+
+*El 'hola' queremos que escriba una vez pero hasta los 1000 milisegundos:
+setTimeout(() => console.log('hola'), 1000);
+
+*Detener el setInterval o contador con clearInterval:
+const contador = setInterval(() => console.log('hola'), 1000);
+clearInterval(contador);
+
+*Date.now es para imprimir en milisegundos la hora que es actualmente:
+const timeStart = Date.now(); ó
+
+Date.now() - timeStart; 
+
+10.0 Te traes el parrafo time del html
+10.1 Haces una variable sin valor 'timeStart'
+10.2 Sino tiene valor le asignas el tiempo de ahora.
+10.3 Haces un afuncion especifica pra el tiempo 'showTime'
+10.4 Que escriba en el parrafo del HTML el tiempo de ahora - startTime de 10.1
+
+10.5 Haces una variable sin valor 'timeInterval'
+10.6 Que repita la funcion showTime de 10.3 por 100 milisegundos
+10.7 Si gana el juego detener el tiempo con clearInterval
+10.8 Si pierde el juego el 'timeStart' ponerlo undefined para que se b00rre, y 
+     el mismo flujo de la  funcion de 'levelFail' de 8.3 activara la funcion startGame() otra vez y 
+     volvera asignarle un  valor en 10.2, se r3pite
+
+# Clase 19
+
+Metodosss
+*Guardar variable en el local Storage   
+localStorage.setItem('patito', valor);
+
+*Llamar la variable guardada
+localStorage.getItem('patito');
+
+*Borrar la variable guardada en el localStorage del navegador
+localStorage.removeItem('patito');
+
+
+11.0 Todavia NO tenemos la variable guardado en localStorage, pero vamos inciar el codigo como sí
+     ya la tuvieramos. Mandamos llamar la variable 'record_time' del localStorage
+11.1 Creamos la variable 'playerTime' que le cuenta el tiempo al jugador, es Date.now del presente men0ss
+     Date.now del pasado 10.2 
+11.2 Sí ya existe un recordTime 11.0
+11.3 Si el recordTime guardado en el localStorage es mayor o igual a PlayerTime, superaste el record y guardas
+     con setItem en la variable 'record_time' guaradas el nuevo valor del PlayerTime.
+11.4 sino No superaste el recordTime
+11.5 Sino hay 'record_time' en localStorage crealo con setItem;
+11.6 Ver recordTime y PlayerTime
+11.7 Te traes el parrafo del record y el resultado
+11.8 Le einsertas en el HTML el letrero de que supero el record
+11.9 Le einsertas en el HTML el letrero de que NO supero el record
+11.10 Le einsertas en el HTML el letrero de que es la primera vez que se guarda un record.
+11.12 Cuando inicie el juego ejectuta la funcion showRecord() para mostrar el record
+11.13 En el HTML inserta lo que haiga en la viarible 'record_time'
+
+
+#Clase 20
+12.0 Cuando se active la funcion 'setCanvasSize' que es reacomodar tamaño de pantalla
+    se borre la posicion de mi jugador y vuelva acomodar al activar startGame() de 1.13
+12.1 Quitandole los decimales al canva size para que haga los calculos mejor
+
+
+     
+
+
+
+   
